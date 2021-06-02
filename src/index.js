9d207024dab631fc145cd58e2488a900debbf118
Ian Milliken <ianmilliken1215@gmail.com>
2021-06-01 22:11:07 -0400

    set up and users

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('hello world')
+const usersList = document.querySelector('#users-list');
+const restaurantsList = document.querySelector('#restaurants-list');
+const reservationsList = document.querySelector('#reservations-list');
+
+// restaurantsList.addEventListener('click', async(ev) => {
+//     if(ev.target.tagName === 'LI') {
+//         const restaurantId = ev.target.getAttribute('data-id');
+//         const 
+//     }
+// })
+
+let users;
+
+const renderUsers = () => {
+    const userId = window.location.hash.slice(1);
+    const html = users.map(user => {
+        return `
+          <li class = '${userId === user.id ? 'selected': ''}'>
+            <a href = '#${ user.id }'>
+            ${user.name}
+            </a>
+          </li>
+        `;
+    }).join('');
+    usersList.innerHTML = html;
+};
+
+const loadUsers = async() => {
+    const response = await fetch('/api/users');
+    users = await response.json();
+    renderUsers();
+};
+
+
+
+window.addEventListener('hashchange', ()=> {
+    renderUsers();
+    
+  });
